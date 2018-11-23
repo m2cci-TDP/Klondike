26507836e0ade613037262c981910c8dbdcead6f
Belhaj Mohammed <Mohammed.belhaj.55@gmail.com>
2018-11-23 12:41:25 +0100

    working MD with reforming/endgame C4 Modification

--- src/C4/C4.c
+++ src/C4/C4.c
@@ -11,92 +11,89 @@ char TexteCouleurC4[5][8] = {"", "Trefle", "Carreau", "Coeur", "Pique"};
 
 
 typedef Tas SerieCouleurC4;
-SerieCouleurC4 LigneC4[DerniereCouleur+1];
+SerieCouleurC4 LigneC4[DerniereCouleur + 1];
 Tas TalonC4;
 /* localisation des tas */
-Localisation LocSeriesC4[DerniereCouleur+1];
+Localisation LocSeriesC4[DerniereCouleur + 1];
 Localisation LocTalonC4;
 
 /* Formation du tableau de jeu initial */
 
-void SaisirLocTasC4()
-{
-  int i;
+void SaisirLocTasC4() {
+    int i;
 
-  LocTalonC4.NC = 1; /* placer le talon dans la premiere case du tableau */
-  LocTalonC4.NL = 1;
+    LocTalonC4.NC = 1; /* placer le talon dans la premiere case du tableau */
+    LocTalonC4.NL = 1;
 
-  for (i=PremiereCouleur; i<=DerniereCouleur; i++)
-  {
-    LocSeriesC4[i].NC = 2*i+2; /* placer la série de 4 tas */
-    LocSeriesC4[i].NL = 1;
-  }
+    for (i = PremiereCouleur; i <= DerniereCouleur; i++) {
+        LocSeriesC4[i].NC = 2 * i + 2; /* placer la série de 4 tas */
+        LocSeriesC4[i].NL = 1;
+    }
 }
 
-void CreerTableauInitialC4() /*creer les le talon + les tas*/
-{
-  Couleur Co;
-  SaisirLocTasC4(); /*placer les 4 tas */
+void CreerTableauInitialC4() /*creer les le talon + les tas*/ {
+    Couleur Co;
+    SaisirLocTasC4(); /*placer les 4 tas */
 
-  /* Création du talon avec un jeu de 32 cartes*/
-  CreerJeuNeuf(32, LocTalonC4, &TalonC4);
-  BattreTas(&TalonC4);
-  /* Création des séries vides de chaque couleur  */
+    /* Création du talon avec un jeu de 32 cartes*/
+    CreerJeuNeuf(32, LocTalonC4, &TalonC4);
+    BattreTas(&TalonC4);
+    /* Création des séries vides de chaque couleur  */
 
-  for (Co=PremiereCouleur; Co<=DerniereCouleur; Co++)
-  {
-    CreerTasVide(LocSeriesC4[Co], etale, &(LigneC4[Co])); /*creer des emplacements de tas vides*/
-  }
+    for (Co = PremiereCouleur; Co <= DerniereCouleur; Co++) {
+        CreerTasVide(LocSeriesC4[Co], etale, &(LigneC4[Co])); /*creer des emplacements de tas vides*/
+    }
 }
 
-void ReformerTableauInitialC4()
-{
-  Couleur Co;
+void ReformerTableauInitialC4() {
+    Couleur Co;
 
-  /* On reforme le talon, en empilant les cartes de la ligne */
+    /* On reforme le talon, en empilant les cartes de la ligne */
+    
+    for (Co = PremiereCouleur; Co <= DerniereCouleur; Co++) {
 
-  for (Co=PremiereCouleur; Co<=DerniereCouleur; Co++)
-  {
-    EmpilerTas(&(LigneC4[Co]));
-    PoserTasSurTas(&(LigneC4[Co]), &TalonC4);
-    EtalerTas(&(LigneC4[Co]));
-  }
-  RetournerTas(&TalonC4);
-  BattreTas(&TalonC4);
+        while (LaHauteur(LigneC4[Co]) != 0) {
+            if (EstDecouverte(CarteSur(LigneC4[Co]))) {
+                RetournerCarteSur(&LigneC4[Co]);
+            }
+            DeplacerHautSur(&LigneC4[Co], &TalonC4);
+        }
+    }
+
+    BattreTas(&TalonC4);
 }
+
 /* Visualisation des états du jeu */
 
 void RetournerSiPasRetourner(Tas *T) { //Retourne les cartes non retournées d'un tas - Utilisée en fin de partie
     int i;
     for (i = LaHauteur(*T); i >= 1; i--) {
-        if (EstCachee(IemeCarte(*T, i))){
+        if (EstCachee(IemeCarte(*T, i))) {
             EchangerCartes(i, LaHauteur(*T), T);
             RetournerCarteSur(T);
             EchangerCartes(i, LaHauteur(*T), T);
         }
     }
 }
-void AfficherC4()
-{
-  Couleur Co;
 
-  EffacerGraphique();
-  AfficherTas(TalonC4, "Talon");
+void AfficherC4() {
+    Couleur Co;
 
-  for (Co=PremiereCouleur; Co<=DerniereCouleur; Co++) {
-    AfficherTas(LigneC4[Co], TexteCouleurC4[Co]);
-  }
+    EffacerGraphique();
+    AfficherTas(TalonC4, "Talon");
 
-  AttendreCliquer();
+    for (Co = PremiereCouleur; Co <= DerniereCouleur; Co++) {
+        AfficherTas(LigneC4[Co], TexteCouleurC4[Co]);
+    }
+
+    AttendreCliquer();
 }
 
-
-void JouerTasC4(Tas *T, Couleur *Co)
-{
-  RetournerCarteSur(T);
-  *Co=LaCouleur(CarteSur(*T));
-  DeplacerHautSous(T, &LigneC4[*Co]);
-  RetournerCarteSous(&LigneC4[*Co]);
+void JouerTasC4(Tas *T, Couleur *Co) {
+    RetournerCarteSur(T);
+    *Co = LaCouleur(CarteSur(*T));
+    DeplacerHautSous(T, &LigneC4[*Co]);
+    RetournerCarteSous(&LigneC4[*Co]);
 }
 
 booleen reussirC4(ModeTrace MT) {
@@ -118,87 +115,73 @@ booleen reussirC4(ModeTrace MT) {
             RetournerSiPasRetourner(&LigneC4[j]);
 
         }
-        
+
     }
     if (MT == AvecTrace) AfficherC4();
     return (Co == DerniereCouleur) && (i == NbCartes / 4);
 }
 
-void JouerC4(ModeTrace MT)
-{
-  if (MT == AvecTrace)
-  {
-    AfficherC4();
-  }
-  Couleur Co2 = PremiereCouleur;
-  Couleur Co;
-  int i;
+void JouerC4(ModeTrace MT) {
+    if (MT == AvecTrace) {
+        AfficherC4();
+    }
+    Couleur Co2 = PremiereCouleur;
+    Couleur Co;
+    int i;
 
-  for(Co=PremiereCouleur; Co<=DerniereCouleur; Co++)
-  {
-    for(i=0; i<NbCartes/4; i++) /* mettre 8 cartes dans chaque tas (distribution) */
-    {
-      DeplacerHautSous(&TalonC4, &(LigneC4[Co]));
+    for (Co = PremiereCouleur; Co <= DerniereCouleur; Co++) {
+        for (i = 0; i < NbCartes / 4; i++) /* mettre 8 cartes dans chaque tas (distribution) */ {
+            DeplacerHautSous(&TalonC4, &(LigneC4[Co]));
+        }
     }
-  }
-  if (MT == AvecTrace)
-  {
-    AfficherC4();
-  }
-  do
-  {
-    RetournerCarteSur(&LigneC4[Co2]);
-    if (MT == AvecTrace)
-    {
-      AfficherC4();
+    if (MT == AvecTrace) {
+        AfficherC4();
     }
-    JouerTasC4(&LigneC4[Co2], &Co); /* jouer le tas courant */
-    Co2 = Co;/* se placer dans le tas de destination */
-    if (MT == AvecTrace)
-    {
-      AfficherC4();
-    }
-  } while (!EstDecouverte(CarteSur(LigneC4[Co2])));
+    do {
+        RetournerCarteSur(&LigneC4[Co2]);
+        if (MT == AvecTrace) {
+            AfficherC4();
+        }
+        JouerTasC4(&LigneC4[Co2], &Co); /* jouer le tas courant */
+        Co2 = Co; /* se placer dans le tas de destination */
+        if (MT == AvecTrace) {
+            AfficherC4();
+        }
+    } while (!EstDecouverte(CarteSur(LigneC4[Co2])));
 }
 
-booleen JouerUneC4(ModeTrace MT)
-{
-  JouerC4(MT);
+booleen JouerUneC4(ModeTrace MT) {
+    JouerC4(MT);
 
-  return reussirC4(MT);
+    return reussirC4(MT);
 }
 
-void ObserverC4(int NP)
-{
-  int i = 0;
+void ObserverC4(int NP) {
+    int i = 0;
 
-  CreerTableauInitialC4();
-  while (i < NP) {
-    if (JouerUneC4(AvecTrace))
-    {
-      printf("Vous avez gagné !\n");
+    CreerTableauInitialC4();
+    while (i < NP) {
+        if (JouerUneC4(AvecTrace)) {
+            printf("Vous avez gagné !\n");
+        } else {
+            printf("Vous avez perdu !\n");
+        }
+        ReformerTableauInitialC4();
+        i++;
     }
-    else
-    {
-      printf("Vous avez perdu !\n");
-    }
-    ReformerTableauInitialC4();
-    i++;
-  }
 }
 
-void AnalyserC4(int NP)
-{
-  int i = 0;
-  pStatsKlondike stats = initStats();
+void AnalyserC4(int NP) {
+    int i = 0;
+    pStatsKlondike stats = initStats();
 
-  CreerTableauInitialC4();
-  while (i < NP) {
-    addStats(&stats, JouerUneC4(SansTrace));
-    ReformerTableauInitialC4();
-    i++;
-  }
+    CreerTableauInitialC4();
+    while (i < NP) {
+        addStats(&stats, JouerUneC4(SansTrace));
+        ReformerTableauInitialC4();
+        i++;
+    }
 
-  pctStats(stats);
-  freeStats(&stats);
+    pctStats(stats);
+    freeStats(&stats);
 }
--- src/MD/MD.c
+++ src/MD/MD.c
@@ -11,34 +11,31 @@ char TexteCouleurMD[5][8] = {"", "Trefle", "Carreau", "Coeur", "Pique"};
 
 
 typedef Tas SerieCouleurMD;
-SerieCouleurMD LigneMD[DerniereCouleur+1];
-Tas Tastock[DerniereCouleur+1];
+SerieCouleurMD LigneMD[DerniereCouleur + 1];
+Tas Tastock[DerniereCouleur + 1];
 Tas TalonMD;
 /* localisation des tas */
-Localisation LocSeriesMD[DerniereCouleur+1];
-Localisation LocSeriesTS[DerniereCouleur+1];
+Localisation LocSeriesMD[DerniereCouleur + 1];
+Localisation LocSeriesTS[DerniereCouleur + 1];
 Localisation LocTalonMD;
 
 /* Formation ducolor tableau de jeu initial */
 
-void SaisirLocTasMD()
-{
+void SaisirLocTasMD() {
     int i;
 
     LocTalonMD.NC = 1; /* placer le talon dans la premiere case du tableau */
     LocTalonMD.NL = 1;
 
-    for (i=PremiereCouleur; i<=DerniereCouleur; i++)
-    {
-        LocSeriesMD[i].NC = 2*i+2; /* placer la série de 4 tas */
+    for (i = PremiereCouleur; i <= DerniereCouleur; i++) {
+        LocSeriesMD[i].NC = 2 * i + 2; /* placer la série de 4 tas */
         LocSeriesMD[i].NL = 1;
-        LocSeriesTS[i].NC = 2*i+2; /* placer la série de 4 tas */
+        LocSeriesTS[i].NC = 2 * i + 2; /* placer la série de 4 tas */
         LocSeriesTS[i].NL = 3;
     }
 }
 
-void CreerTableauInitialMD() /*creer les le talon + les tas*/
-{
+void CreerTableauInitialMD() /*creer les le talon + les tas*/ {
     Couleur Co;
     SaisirLocTasMD(); /*placer les 4 tas */
 
@@ -47,87 +44,80 @@ void CreerTableauInitialMD() /*creer les le talon + les tas*/
     BattreTas(&TalonMD);
     /* Création des séries vides de chaque couleur  */
 
-    for (Co=PremiereCouleur; Co<=DerniereCouleur; Co++)
-    {
+    for (Co = PremiereCouleur; Co <= DerniereCouleur; Co++) {
         CreerTasVide(LocSeriesMD[Co], empile, &(LigneMD[Co])); /*creer des emplacements de tas vides*/
         CreerTasVide(LocSeriesTS[Co], empile, &(Tastock[Co])); //Creation des emplacements de tas de stock
     }
 }
 
-//void ReformerTableauInitialMD()
-//{
-//  Couleur Co;
-//
-//  /* On reforme le talon, en empilant les cartes de la ligne */
-//  for (Co=PremiereCouleur; Co<=DerniereCouleur; Co++)
-//  {
-//    /*
-//    EmpilerTas(&(LigneC4[Co]));
-//    PoserTasSurTas(&(LigneC4[Co]), &TalonC4);
-//    EtalerTas(&(LigneC4[Co]));
-//    */
-//    /* fix bug de retournement */
-//    while (LaHauteur(LigneC4[Co]) != 0) {
-//      if (EstDecouverte(CarteSur(LigneC4[Co]))) {
-//        RetournerCarteSur(&LigneC4[Co]);
-//      }
-//      DeplacerHautSur(&LigneC4[Co], &TalonC4);
-//    }
-//  }
-//  /* RetournerTas(&TalonC4); */
-//  BattreTas(&TalonC4);
-//}
+void ReformerTableauInitialMD() {
+    Couleur Co;
 
-/* fix bug DeplacerBasSur(T, T)
-A corriger dans Tas.c */
+    /* On reforme le talon, en empilant les cartes de la ligne et des tas de stockage*/
+    if (!TasVide(TalonMD) && EstDecouverte(CarteSur(TalonMD))) {
+        RetournerCarteSur(&TalonMD);
+    }
+    for (Co = PremiereCouleur; Co <= DerniereCouleur; Co++) {
+        while (LaHauteur(LigneMD[Co]) != 0) {
+            if (EstDecouverte(CarteSur(LigneMD[Co]))) {
+                RetournerCarteSur(&LigneMD[Co]);
+            }
+            DeplacerHautSur(&LigneMD[Co], &TalonMD);
+
+        }
+        while (LaHauteur(Tastock[Co]) != 0) {
+            if (EstDecouverte(CarteSur(Tastock[Co]))) {
+                RetournerCarteSur(&Tastock[Co]);
+            }
+            DeplacerHautSur(&Tastock[Co], &TalonMD);
+        }
+    }
+    /* RetournerTas(&TalonC4); */
+    BattreTas(&TalonMD);
+}
 
 /* Visualisation des états du jeu */
 
-void Monter(Tas *T)
-{
+void Monter(Tas *T) {
     Couleur Co = LaCouleur(CarteSur(*T));
     DeplacerHautSur(T, &LigneMD[Co]);
 }
 
-booleen Descendre (Tas *T){
+booleen Descendre(Tas *T) {
     Rang R;
-    R=LeRang(CarteSur(*T));
-    int i,j;
-    j=0;
+    R = LeRang(CarteSur(*T));
+    int i, j;
+    j = 0;
     int X;
     int tempo[4];
-    for (i=1;i<5;i++){
-        if (!TasVide(Tastock[i])){
-            tempo[i-1]=LeRang(CarteSur(Tastock[i]))-R;
-            if (tempo[i-1]<0) tempo[i-1]=100;
-        }
-        else {
-        tempo[i-1]=10;
-        }
-    }
-    X=tempo[0];
-    for (i=1;i<4;i++){
-        if (tempo[i]<X) {
-        X=tempo[i];
-        j=i;
+    for (i = 1; i < 5; i++) {
+        if (!TasVide(Tastock[i])) {
+            tempo[i - 1] = LeRang(CarteSur(Tastock[i])) - R;
+            if (tempo[i - 1] < 0) tempo[i - 1] = 100;
+        } else {
+            tempo[i - 1] = 10;
         }
     }
-    if (X != 100){
-        DeplacerHautSur(T,&Tastock[j+1]);}
-    return X!=100;
+    X = tempo[0];
+    for (i = 1; i < 4; i++) {
+        if (tempo[i] < X) {
+            X = tempo[i];
+            j = i;
+        }
+    }
+    if (X != 100) {
+        DeplacerHautSur(T, &Tastock[j + 1]);
+    }
+    return X != 100;
 }
 
-
-
-void AfficherMD()
-{
+void AfficherMD() {
     Couleur Co;
 
     EffacerGraphique();
     AfficherTas(TalonMD, "Talon");
 
-    for (Co=PremiereCouleur; Co<=DerniereCouleur; Co++)
-    {
+    for (Co = PremiereCouleur; Co <= DerniereCouleur; Co++) {
         AfficherTas(LigneMD[Co], TexteCouleurMD[Co]);
         AfficherTas(Tastock[Co], "");
 
@@ -136,78 +126,61 @@ void AfficherMD()
     AttendreCliquer();
 }
 
-
-booleen MonteStock()
-{
-    int i=1;
+booleen MonteStock() {
+    int i = 1;
     Couleur Co;
     booleen OK = faux;
-    while (i<5 && !OK){
-        if (TasVide(Tastock[i])){
-		i++;
-	}
-	else {
-		Co=LaCouleur(CarteSur(Tastock[i]));
-		if (TasVide(LigneMD[Co]) || (RangSuivant(LeRang(CarteSur(LigneMD[Co])))!=LeRang(CarteSur(Tastock[i])))) {
-			i++;
-		}
-		else {
-			OK = vrai;
-			Monter(&Tastock[i]);
-		}
-	}
-     }
-     return OK;
-       
+    while (i < 5 && !OK) {
+        if (TasVide(Tastock[i])) {
+            i++;
+        } else {
+            Co = LaCouleur(CarteSur(Tastock[i]));
+            if (TasVide(LigneMD[Co]) || (RangSuivant(LeRang(CarteSur(LigneMD[Co]))) != LeRang(CarteSur(Tastock[i])))) {
+                i++;
+            } else {
+                OK = vrai;
+                Monter(&Tastock[i]);
+            }
+        }
+    }
+    return OK;
+
 
 }
 
-booleen jouerMD(ModeTrace MT)
-{
+booleen JouerUneMD(ModeTrace MT) {
     Couleur Co;
     booleen OK;
     booleen OK2 = vrai;
-    if (MT == AvecTrace)
-    {
+    if (MT == AvecTrace) {
         AfficherMD();
     }
-    while (!TasVide(TalonMD) && OK2)
-    {
+    while (!TasVide(TalonMD) && OK2) {
         OK = vrai;
         RetournerCarteSur(&TalonMD);
-        if (MT == AvecTrace)
-        {
+        if (MT == AvecTrace) {
             AfficherMD();
         }
         Co = LaCouleur(CarteSur(TalonMD));
-        if ((LeRang(CarteSur(TalonMD)) == PremierRang) || (!TasVide(LigneMD[Co])&& RangSuivant(LeRang(CarteSur(LigneMD[Co])))==LeRang(CarteSur(TalonMD))))
-        {
+        if ((LeRang(CarteSur(TalonMD)) == PremierRang) || (!TasVide(LigneMD[Co]) && RangSuivant(LeRang(CarteSur(LigneMD[Co]))) == LeRang(CarteSur(TalonMD)))) {
             Monter(&TalonMD);
-            if (MT == AvecTrace)
-            {
+            if (MT == AvecTrace) {
                 AfficherMD();
             }
-            while (OK && OK2)
-            {
-                OK=MonteStock();
-                if (MT == AvecTrace)
-                {
+            while (OK && OK2) {
+                OK = MonteStock();
+                if (MT == AvecTrace && OK) {
                     AfficherMD();
                 }
             }
-        }
-        else
-        {
-            OK2=Descendre(&TalonMD);
-            if (MT == AvecTrace)
-            {
+        } else {
+            OK2 = Descendre(&TalonMD);
+            if (MT == AvecTrace) {
                 AfficherMD();
             }
-            while (OK && OK2)
-            {
-                OK=MonteStock();
-                if (MT == AvecTrace)
-                {
+            while (OK && OK2) {
+                OK = MonteStock();
+                if (MT == AvecTrace && OK) {
                     AfficherMD();
                 }
             }
@@ -215,47 +188,36 @@ booleen jouerMD(ModeTrace MT)
 
         }
     }
-    if (TasVide(TalonMD)) {return vrai;}
-    else return faux;
+    if (TasVide(TalonMD)) {
+        return vrai;
+    } else return faux;
 }
 
 
 
-booleen JouerUneMD(ModeTrace MT)
-{
-    return jouerMD(MT);
-}
-
-void ObserverMD(int NP)
-{
+void ObserverMD(int NP) {
     int i = 0;
 
     CreerTableauInitialMD();
-    while (i < NP)
-    {
-        if (JouerUneMD(AvecTrace))
-        {
+    while (i < NP) {
+        if (JouerUneMD(AvecTrace)) {
             printf("Vous avez gagné !\n");
-        }
-        else
-        {
+        } else {
             printf("Vous avez perdu !\n");
         }
-        //ReformerTableauInitialC4();
+        ReformerTableauInitialMD();
         i++;
     }
 }
 
-void AnalyserMD(int NP)
-{
+void AnalyserMD(int NP) {
     int i = 0;
     pStatsKlondike stats = initStats();
 
     CreerTableauInitialMD();
-    while (i < NP)
-    {
+    while (i < NP) {
         addStats(&stats, JouerUneMD(SansTrace));
-        //ReformerTableauInitialC4();
+        ReformerTableauInitialMD();
         i++;
     }
 
